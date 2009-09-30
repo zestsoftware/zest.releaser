e9facf3178e3fa0aa1063df41aa482f54fad6c0c
Reinout van Rees <reinout@vanrees.org>
2009-09-30 20:44:09 +0000

    started functional tests
    svn path=/zest.releaser/trunk/; revision=98593

--- zest/releaser/tests/functional.py
+++ zest/releaser/tests/functional.py
@@ -1,11 +1,38 @@
 """Set up functional test fixtures"""
+import commands
+import os
 import shutil
 import tempfile
+import pkg_resources
+import tarfile
 
 
 def setup(test):
     test.tempdir = tempfile.mkdtemp()
-    test.globs.update({'tempdir': test.tempdir})
+    # TODO: check if svnadmin is installed and set variable accordingly.
+    # Init svn repo.
+    repodir = os.path.join(test.tempdir, 'svnrepo')
+    commands.getoutput('svnadmin create %s' % repodir)
+    repo_url = 'file://' + repodir # TODO: urllib or so for windows
+    # Prepare and check in example project
+    # Note: extractall only exists in python2.5
+    example_tar = pkg_resources.resource_filename(
+        'zest.releaser.tests', 'example.tar')
+    tarfile.TarFile(example_tar).extractall(path=test.tempdir)
+    sourcedir = os.path.join(test.tempdir, 'tha.example')
+    commands.getoutput('svn mkdir %s/tha.example -m "mkdir"' % repo_url)
+    commands.getoutput('svn mkdir %s/tha.example/tags -m "mkdir"' % repo_url)
+    commands.getoutput(
+        'svn import %s %s/tha.example/trunk -m "import"' % (sourcedir,
+                                                            repo_url))
+    # Zap the source dir and make it a checkout
+    shutil.rmtree(sourcedir)
+    commands.getoutput(
+        'svn co %s/tha.example/trunk %s' % (repo_url, sourcedir))
+
+    test.globs.update({'tempdir': test.tempdir,
+                       'repo_url': repo_url,
+                       'sourcedir': sourcedir})
 
 
 def teardown(test):
