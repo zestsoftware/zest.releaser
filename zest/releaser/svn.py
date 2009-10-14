d81ca858f3dfdcc70cc7f9d49620a4420d51c16f
Reinout van Rees <reinout@vanrees.org>
2009-10-14 08:46:27 +0000

    hg coverage at 100% and added quite some svn tests
    svn path=/zest.releaser/trunk/; revision=99513

--- zest/releaser/svn.py
+++ zest/releaser/svn.py
@@ -71,6 +71,7 @@ class Subversion(BaseVersionControl):
             logger.error('Network problem: %s', tag_info)
             sys.exit()
         tags = [line.replace('/', '') for line in tag_info.split('\n')]
+        tags = [tag for tag in tags if tag] # filter empty ones
         logger.debug("Available tags: %r", tags)
         return tags
 
