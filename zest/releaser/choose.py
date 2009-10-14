1346a81c73b3851b51a288b9f2e51053475dcdd4
Reinout van Rees <reinout@vanrees.org>
2009-10-14 10:15:17 +0000

    testing choose.py and fixed a bug: no directory recursion needed, just look in the current directory for .svn and .hg files
    svn path=/zest.releaser/trunk/; revision=99532

--- zest/releaser/choose.py
+++ zest/releaser/choose.py
@@ -10,11 +10,11 @@ logger = logging.getLogger('chooser')
 def version_control():
     """Return an object that provides the version control interface based
     on the detected version control system."""
-    for dirpath, dirnames, filenames in os.walk('.'):
-        if '.svn' in dirnames:
-            return svn.Subversion()
-        elif '.hg' in dirnames:
-            return hg.Hg()
+    curdir_contents = os.listdir('.')
+    if '.svn' in curdir_contents:
+        return svn.Subversion()
+    elif '.hg' in curdir_contents:
+        return hg.Hg()
     else:
         logger.critical('No version control system detected.')
         sys.exit(1)
