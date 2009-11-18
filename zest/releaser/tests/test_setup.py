59f865ab6c716753f7f7ba870fdd2d9eec1d47ad
Godefroid Chapelle <gotcha@bubblenet.be>
2009-11-18 18:08:52 +0000

    seems .pypirc is case insensitive
    svn path=/zest.releaser/trunk/; revision=102666

--- zest/releaser/tests/test_setup.py
+++ zest/releaser/tests/test_setup.py
@@ -1,4 +1,11 @@
 import z3c.testsetup
 
 
-test_suite = z3c.testsetup.register_all_tests('zest.releaser')
+import re
+from zope.testing import renormalizing
+checker = renormalizing.RENormalizing([
+    (re.compile('[Pp][Yy][Pp][Ii]'), 'pypi'),
+    ])
+
+
+test_suite = z3c.testsetup.register_all_tests('zest.releaser', checker=checker)
