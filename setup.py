627a8a927d1439c4073a14f24b4c192b941f38a8
Maurits van Rees <maurits@vanrees.org>
2010-03-17 21:28:14 +0000

    Got rid of ugly setup.py hack with UltraMagicString that was meant to avoid encoding errors when registering this package at pypi but which was not working for python2.4 (at least with collective.dist). Only ascii is allowed in the long_description if you want to avoid problems at one point or another.
    svn path=/zest.releaser/trunk/; revision=113346

--- setup.py
+++ setup.py
@@ -5,31 +5,6 @@ import os
 version = '3.6dev'
 
 
-# Adapted from
-# http://stackoverflow.com/questions/1162338/whats-the-right-way-to-use-unicode-metadata-in-setup-py
-class UltraMagicString(object):
-    # Catch-22:
-    # - if I return Unicode, python setup.py --long-description as well
-    #   as python setup.py upload fail with a UnicodeEncodeError
-    # - if I return UTF-8 string, python setup.py sdist register
-    #   fails with an UnicodeDecodeError
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-    def __unicode__(self):
-        return self.value.decode('utf-8')
-
-    def __add__(self, other):
-        return UltraMagicString(self.value + str(other))
-
-    def split(self, *args, **kw):
-        return self.value.split(*args, **kw)
-
-
 def read(filename):
     filename = os.path.join('zest', 'releaser', filename)
     return unicode(codecs.open(filename, encoding='utf-8').read())
@@ -39,17 +14,16 @@ def read_from_here(filename):
     return unicode(codecs.open(filename, encoding='utf-8').read())
 
 
-long_description = u'\n\n'.join([read('README.txt'),
-                                 read('entrypoints.txt'),
-                                 read('TODO.txt'),
-                                 read('CREDITS.txt'),
-                                 read_from_here('CHANGES.txt')])
-
+long_description = '\n\n'.join([read('README.txt'),
+                                read('entrypoints.txt'),
+                                read('TODO.txt'),
+                                read('CREDITS.txt'),
+                                read_from_here('CHANGES.txt')])
 
 setup(name='zest.releaser',
       version=version,
       description="Software releasing made easy and repeatable",
-      long_description=UltraMagicString(long_description.encode('utf-8')),
+      long_description=long_description,
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
