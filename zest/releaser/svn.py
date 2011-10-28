88b6b302272f1fa7b63f72f84f421f08ff8b75b8
Maurits van Rees <maurits@vanrees.org>
2011-10-28 15:00:51 +0200

    Fixed finding a history/changes file that is in a sub directory when using subversion 1.7 (or higher).
    Do this by comparing the version returned by 'svn --version --quiet' with 1.7.

--- zest/releaser/svn.py
+++ zest/releaser/svn.py
@@ -1,6 +1,7 @@
 import tempfile
 import logging
 import sys
+from distutils.version import LooseVersion
 
 from zest.releaser import utils
 from zest.releaser.utils import system
@@ -12,7 +13,20 @@ logger = logging.getLogger('zest.releaser')
 class Subversion(BaseVersionControl):
     """Command proxy for Subversion"""
     internal_filename = '.svn'
-    spreaded_internal = True
+
+    @property
+    def spreaded_internal(self):
+        """Are the .svn directories spread all over the place?"""
+        version = system('svn --version --quiet')
+        try:
+            version = LooseVersion(version)
+        except ValueError:
+            # Can't interpret this version, fall back to True.
+            return True
+        if version >= LooseVersion('1.7'):
+            # svn 1.7 or higher only has a top-level .svn directory
+            return False
+        return True
 
     def _svn_info(self):
         """Return svn url"""
