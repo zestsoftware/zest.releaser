zestsoftware/zest.releaser

commit 552df6675e40e6c4abf6d60681413e1611b1c3a0

python3 if/else

setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup, find_packages
+from __future__ import unicode_literals
 import codecs
+
+from setuptools import setup, find_packages
 
 version = '4.1.dev0'
 
 
 def read(filename):
-    return unicode(codecs.open(filename, encoding='utf-8').read())
+    try:
+        return unicode(codecs.open(filename, encoding='utf-8').read())
+    except NameError:
+        # python 3, perhaps six can handle this more elegantly.
+        return open(filename).read()
+
 
 
 long_description = '\n\n'.join([read('README.rst'),

commit 2082afe8cd4cf1608951824f8abc8777b2839975

Fix setup.py encoding for Python 2 / 3. Arguments should be ``str`` in both versions. Fixes bug introduced in 552df66. #113.

setup.py
@@ -1,7 +1,9 @@
-from __future__ import unicode_literals
 import codecs
+import sys
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 
 version = '5.2.dev0'
 
@@ -10,14 +12,17 @@ def read(filename):
     try:
         return unicode(codecs.open(filename, encoding='utf-8').read())
     except NameError:
-        # python 3, perhaps six can handle this more elegantly.
         return open(filename, 'r', encoding='utf-8').read()
 
 
 
-long_description = '\n\n'.join([read('README.rst'),
-                                read('CREDITS.rst'),
-                                read('CHANGES.rst')])
+long_description = u'\n\n'.join([read('README.rst'),
+                                 read('CREDITS.rst'),
+                                 read('CHANGES.rst')])
+
+if sys.version_info < (3,):
+    long_description = long_description.encode('utf-8')
+
 
 setup(name='zest.releaser',
       version=version,