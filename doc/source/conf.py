96f0f3e441c08c40c062cf92c44face5bb18b4d5
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-11-11 13:24:36 +0100

    Build doc: Use sphinx_rtd_theme when building docs locally.
    This eases verifying any locally build documentation.
    
    [ci skip]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,6 +6,9 @@
 
 import datetime
 import pkg_resources
+import os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 project = "zest.releaser"
 author = "Reinout and Maurits van Rees"
@@ -79,7 +82,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
