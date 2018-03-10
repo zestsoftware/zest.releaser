9361d3811bfec698457b6a370a1ff034abf1b45e
Maurits van Rees <maurits@vanrees.org>
2018-03-10 13:37:54 +0100

    Let subprocess.Popen work in Python 2 and 3.
    Only in Python 3 can we use it as context manager to avoid unclosed files.

--- zest/releaser/utils.py
+++ zest/releaser/utils.py
@@ -691,6 +691,37 @@ def format_command(command):
 __command_is_string__ = False
 
 
+def _subprocess_open(p, command, input_value, show_stderr):
+    i, o, e = (p.stdin, p.stdout, p.stderr)
+    if input_value:
+        i.write(input_value.encode(INPUT_ENCODING))
+    i.close()
+    stdout_output = o.read()
+    stderr_output = e.read()
+    # We assume that the output from commands we're running is text.
+    if not isinstance(stdout_output, six.text_type):
+        stdout_output = stdout_output.decode(OUTPUT_ENCODING)
+    if not isinstance(stderr_output, six.text_type):
+        stderr_output = stderr_output.decode(OUTPUT_ENCODING)
+    # TODO.  Note that the returncode is always None, also after
+    # running p.kill().  The shell=True may be tripping us up.  For
+    # some ideas, see http://stackoverflow.com/questions/4789837
+    if p.returncode or show_stderr or 'Traceback' in stderr_output:
+        # Some error occured
+        result = stdout_output + get_errors(stderr_output)
+    else:
+        # Only return the stdout. Stderr only contains possible
+        # weird/confusing warnings that might trip up extraction of version
+        # numbers and so.
+        result = stdout_output
+        if stderr_output:
+            logger.debug("Stderr of running command '%s':\n%s",
+                         format_command(command), stderr_output)
+    o.close()
+    e.close()
+    return result
+
+
 def _execute_command(command, input_value=''):
     """commands.getoutput() replacement that also works on windows"""
     # Enforce the command to be a list or arguments, except if
@@ -709,40 +740,25 @@ def _execute_command(command, input_value=''):
     else:
         env = None
         show_stderr = True
-    with subprocess.Popen(command,
-                         shell=not isinstance(command, (list, tuple)),
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         close_fds=MUST_CLOSE_FDS,
-                         env=env) as p:
-        i, o, e = (p.stdin, p.stdout, p.stderr)
-        if input_value:
-            i.write(input_value.encode(INPUT_ENCODING))
-        i.close()
-        stdout_output = o.read()
-        stderr_output = e.read()
-        # We assume that the output from commands we're running is text.
-        if not isinstance(stdout_output, six.text_type):
-            stdout_output = stdout_output.decode(OUTPUT_ENCODING)
-        if not isinstance(stderr_output, six.text_type):
-            stderr_output = stderr_output.decode(OUTPUT_ENCODING)
-        # TODO.  Note that the returncode is always None, also after
-        # running p.kill().  The shell=True may be tripping us up.  For
-        # some ideas, see http://stackoverflow.com/questions/4789837
-        if p.returncode or show_stderr or 'Traceback' in stderr_output:
-            # Some error occured
-            result = stdout_output + get_errors(stderr_output)
-        else:
-            # Only return the stdout. Stderr only contains possible
-            # weird/confusing warnings that might trip up extraction of version
-            # numbers and so.
-            result = stdout_output
-            if stderr_output:
-                logger.debug("Stderr of running command '%s':\n%s",
-                             format_command(command), stderr_output)
-        o.close()
-        e.close()
+    # On Python 3, subprocess.Popen can and should be used as context
+    # manager, to avoid unclosed files.  On Python 2 this is not possible.
+    process_kwargs = {
+        'shell': not isinstance(command, (list, tuple)),
+        'stdin': subprocess.PIPE,
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.PIPE,
+        'close_fds': MUST_CLOSE_FDS,
+        'env': env,
+    }
+    if hasattr(subprocess.Popen, '__exit__'):
+        # Python 3
+        with subprocess.Popen(command, **process_kwargs) as process:
+            result = _subprocess_open(
+                process, command, input_value, show_stderr)
+    else:
+        # Python 2
+        process = subprocess.Popen(command, **process_kwargs)
+        result = _subprocess_open(process, command, input_value, show_stderr)
     return result
 
 
