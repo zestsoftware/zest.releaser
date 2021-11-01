704147d83454db5add1286d6b369fc6899abc0bd
Maurits van Rees <maurits@vanrees.org>
2021-11-01 18:00:54 +0100

    Further cleanup of the test setup normalization regexps.

--- zest/releaser/tests/test_setup.py
+++ zest/releaser/tests/test_setup.py
@@ -22,37 +22,24 @@ checker = renormalizing.RENormalizing([
     (re.compile(r'[0-9a-f]{7}\.\.[0-9a-f]{7} [0-9a-f]{6}'),
      '1234567..890abcd ef0123'),
     # .pypirc seems to be case insensitive
-    (re.compile('[Pp][Yy][Pp][Ii]'), 'pypi'),
+    (re.compile(r'[Pp][Yy][Pp][Ii]'), 'pypi'),
     # Normalize tempdirs.  For this to work reliably, we need to use a prefix
     # in all tempfile.mkdtemp() calls.
     (re.compile(
-        '/private%s/testtemp[^/]+' % re.escape(tempfile.gettempdir())),
+        r'/private%s/testtemp[^/]+' % re.escape(tempfile.gettempdir())),
      'TESTTEMP'),  # OSX madness
     (re.compile(
-        '%s/testtemp[^/]+' % re.escape(tempfile.gettempdir())),
+        r'%s/testtemp[^/]+' % re.escape(tempfile.gettempdir())),
      'TESTTEMP'),
     (re.compile(re.escape(tempfile.gettempdir())),
      'TMPDIR'),
-    # git before 1.7.9.2 reported 0 deletions when committing:
-    (re.compile(r', 0 deletions\(-\)'), ''),
-    # Change in git 1.7.9.2: '1 files changed':
-    (re.compile(' 1 files changed'), ' 1 file changed'),
-    # Change in git 1.8.0:
-    (re.compile(r'nothing to commit \(working directory clean\)'),
-     'nothing to commit, working directory clean'),
     # Change in git 2.9.1:
-    (re.compile('nothing to commit, working directory clean'),
+    (re.compile(r'nothing to commit, working directory clean'),
      'nothing to commit, working tree clean'),
-    # Change in git 1.8.5, the hash is removed:
-    (re.compile('# On branch'),
-     'On branch'),
     # We should ignore coloring by colorama.  Or actually print it
     # clearly.  This catches Fore.RED and Fore.MAGENTA.
-    # Note the extra backslash in front of the left bracket, otherwise
-    # you get: "error: unexpected end of regular expression"
     (re.compile(re.escape(Fore.RED)), 'RED '),
     (re.compile(re.escape(Fore.MAGENTA)), 'MAGENTA '),
-    (re.compile('FileNotFoundError'), 'IOError'),
 ])
 
 test_suite = z3c.testsetup.register_all_tests('zest.releaser', checker=checker)
