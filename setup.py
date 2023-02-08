a6cb61093bfb34b092b6215cba1e2079ed0a79d5
Maurits van Rees <maurits@vanrees.org>
2023-02-08 21:43:56 +0100

    Moved long_description to setup.cfg metadata.
    In setup.py this used compatibility code for Python 2, and encoding fallbacks, which are ugly,
    pyproject.toml cannot handle multiple files, as far as I can see.
    setup.cfg happily does this.

--- setup.py
+++ setup.py
@@ -1,29 +1,8 @@
 from setuptools import find_packages
 from setuptools import setup
 
-import codecs
-import sys
-
-
-def read(filename):
-    try:
-        with codecs.open(filename, encoding="utf-8") as f:
-            return f.read()
-    except NameError:
-        with open(filename, encoding="utf-8") as f:
-            return f.read()
-
-
-long_description = "\n\n".join(
-    [read("README.rst"), read("CREDITS.rst"), read("CHANGES.rst")]
-)
-
-if sys.version_info < (3,):
-    long_description = long_description.encode("utf-8")
-
 
 setup(
-    long_description=long_description,
     packages=find_packages(),
     namespace_packages=["zest"],
     include_package_data=True,
